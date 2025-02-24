c23ab9b5e5eefde71a3826980836ea642d50fd5b
Kauã Haymon <kauahaymon@hotmail.com>
2025-02-24 00:14:50 -0400

    Get product from database

--- produtosapi/src/main/java/com/world/haymon/produtosapi/controller/ProductController.java
+++ produtosapi/src/main/java/com/world/haymon/produtosapi/controller/ProductController.java
@@ -2,11 +2,9 @@ package com.world.haymon.produtosapi.controller;
 
 import com.world.haymon.produtosapi.model.Product;
 import com.world.haymon.produtosapi.repository.ProductRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -21,12 +19,15 @@ public class ProductController {
 
     @PostMapping
     public Product save(@RequestBody Product prod) {
-        System.out.println("Produto salvo: " + prod);
-
         var id = UUID.randomUUID().toString(); // Generate Unique Codes
         prod.setId(id);
 
         productRepository.save(prod);
         return prod;
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable("id") String id) {
+        return productRepository.findById(id).orElse(null);
     }
 }

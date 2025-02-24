49cfbe88d4ce1e9369c0a5e9fd818b1785871ace
Kauã Haymon <kauahaymon@hotmail.com>
2025-02-24 15:33:09 -0400

    fix: imports

--- produtosapi/src/main/java/com/world/haymon/produtosapi/ProdutosApiApplication.java
+++ produtosapi/src/main/java/com/world/haymon/produtosapi/ProdutosApiApplication.java
@@ -2,12 +2,6 @@ package com.world.haymon.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class ProdutosApiApplication {
@@ -15,5 +9,4 @@ public class ProdutosApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
 	}
-
 }

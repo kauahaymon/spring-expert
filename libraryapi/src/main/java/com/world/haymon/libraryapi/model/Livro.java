6c197f1e309a2fb1d2901715f99ba74fb32fc910
Kauã Haymon <kauahaymon@hotmail.com>
2025-02-27 22:48:43 -0400

    [ORM] Entidade Livro

---
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/model/Livro.java
@@ -0,0 +1,45 @@
+package com.world.haymon.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro", schema = "public")
+@Data
+/* @Date incorporates ->
+        @Getter@Setter
+        @ToString
+        @EqualsAndHashCode
+        @RequiredArgsConstructor
+ */
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private BigDecimal preco;
+
+    @ManyToOne//(cascade = CascadeType.ALL) // Many "Livros" to one "Autor"
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}

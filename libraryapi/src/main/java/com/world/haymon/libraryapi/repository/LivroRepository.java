299b6fb0490217251ccbd3f808691d433f0254de
Kauã Haymon <kauahaymon@hotmail.com>
2025-02-27 22:49:33 -0400

    add: repositories Autor, Livro

---
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.world.haymon.libraryapi.repository;
+
+import com.world.haymon.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}

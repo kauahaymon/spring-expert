bb75aa660550991de09df7a1100549006bf48db2
Kauã Haymon <kauahaymon@hotmail.com>
2025-02-27 22:46:50 -0400

    [bean] datasource custom configuration Hikari

---
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/config/DatasourceConfiguration.java
@@ -0,0 +1,56 @@
+package com.world.haymon.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    // Db config
+    // @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+    // Connection Pool with Hikari
+    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10); // Max of available connections
+        config.setMinimumIdle(1); // Initial pool size
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000); // Milliseconds (10 minutes)
+        config.setConnectionTimeout(100000); // Timeout to get a connection (could throw error)
+        config.setConnectionTestQuery("select 1");
+
+        return new HikariDataSource(config);
+    }
+}

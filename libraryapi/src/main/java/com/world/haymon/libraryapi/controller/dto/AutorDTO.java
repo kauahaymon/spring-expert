7dfffe3cbf6944c4632f26ca8221675ffe72bf87
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-03 01:14:05 -0400

    [rest] Obter detalhes do Autor

--- libraryapi/src/main/java/com/world/haymon/libraryapi/controller/dto/AutorDTO.java
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/controller/dto/AutorDTO.java
@@ -3,8 +3,10 @@ package com.world.haymon.libraryapi.controller.dto;
 import com.world.haymon.libraryapi.model.Autor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record AutorDTO(
+        UUID id,
         String nome,
         LocalDate dataNascimento,
         String nacionalidade) {

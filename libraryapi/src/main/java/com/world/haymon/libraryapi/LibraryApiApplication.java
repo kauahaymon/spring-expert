8b4fbd1b1b3c0815a558b94fab6f6b406cd403eb
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-02 23:25:08 -0400

    [rest] Configurando Auditoria com JPA

--- libraryapi/src/main/java/com/world/haymon/libraryapi/LibraryApiApplication.java
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/LibraryApiApplication.java
@@ -2,8 +2,10 @@ package com.world.haymon.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LibraryApiApplication {
 
 	public static void main(String[] args) {
--- libraryapi/src/main/java/com/world/haymon/libraryapi/model/Autor.java
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/model/Autor.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +18,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(exclude = "livros")
+@EntityListeners(AuditingEntityListener.class)
 public class Autor {
 
     @Id
@@ -33,4 +38,15 @@ public class Autor {
     // Not a column
     @OneToMany(mappedBy = "autor", fetch = FetchType.LAZY) // One "Autor" to Many "Livros"
     private List<Livro> livros;
+
+    @CreatedDate ///  Persists with Current Date
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate /// Persists Date of Last modification
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
 }

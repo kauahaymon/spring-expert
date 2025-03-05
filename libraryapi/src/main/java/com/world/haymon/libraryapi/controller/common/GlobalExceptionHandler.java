37a993f01a83bc92dcdf43ddd8c09006bfc4af61
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-05 15:28:29 -0400

    [exception handler] Campo invalido, Operacao nao permitida, Registro duplicado

--- libraryapi/src/main/java/com/world/haymon/libraryapi/controller/common/GlobalExceptionHandler.java
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.world.haymon.libraryapi.controller.common;
 
 import com.world.haymon.libraryapi.controller.dto.ErroCampo;
 import com.world.haymon.libraryapi.controller.dto.ErroResposta;
+import com.world.haymon.libraryapi.exceptions.CampoInvalidoException;
+import com.world.haymon.libraryapi.exceptions.OperacaoNaoPermitidaException;
+import com.world.haymon.libraryapi.exceptions.RegistroDuplicadoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -30,5 +33,35 @@ public class GlobalExceptionHandler {
                 HttpStatus.UNPROCESSABLE_ENTITY.value(),
                 "Erro de validação",
                 listErros);
+    }
+
+    @ExceptionHandler(RegistroDuplicadoException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErroResposta handleRegistroDuplicadoException(RegistroDuplicadoException e) {
+        return ErroResposta.conflito(e.getMessage());
+    }
+
+    @ExceptionHandler(OperacaoNaoPermitidaException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErroResposta handleOperacaoNaoPermitidaException(OperacaoNaoPermitidaException e) {
+        return ErroResposta.respostaPadrao(e.getMessage());
+    }
+
+    @ExceptionHandler(CampoInvalidoException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleCampoInvalidoException(CampoInvalidoException e) {
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação",
+                List.of( new ErroCampo( e.getCampo(), e.getMessage()) ));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErroResposta handleErrosNaoTratados(RuntimeException e) {
+        return new ErroResposta(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Ocorreu um erro inesperado. Tente novamente mais tarte.",
+                List.of());
     }
 }

40a31bcde7031bb4362b2b6cd3c1420b602fdc5f
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-13 15:34:17 -0400

    [Open Api] adicionando seguranca na documentacao com bearer authentication

--- libraryapi/src/main/java/com/world/haymon/libraryapi/config/OpenApiConfiguration.java
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/config/OpenApiConfiguration.java
@@ -1,7 +1,11 @@
 package com.world.haymon.libraryapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,7 +13,17 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "Library API",
                 version = "v1"
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
 }

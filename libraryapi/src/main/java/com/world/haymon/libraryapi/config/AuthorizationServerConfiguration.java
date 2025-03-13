beb7cb60e86b28ed14aff3ba4a49b13f0f7065f3
Kauã Haymon <kauahaymon@hotmail.com>
2025-03-13 13:46:42 -0400

    [security] customizando as claims do token jwt

--- libraryapi/src/main/java/com/world/haymon/libraryapi/config/AuthorizationServerConfiguration.java
+++ libraryapi/src/main/java/com/world/haymon/libraryapi/config/AuthorizationServerConfiguration.java
@@ -5,19 +5,25 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.world.haymon.libraryapi.security.CustomAuthentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.KeyPair;
@@ -25,6 +31,8 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -123,5 +131,27 @@ public class AuthorizationServerConfiguration {
                 .oidcLogoutEndpoint("/oauth2/logout")
 
                 .build();
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+        return context -> {
+            Authentication principal = context.getPrincipal();
+            if (principal instanceof CustomAuthentication authentication) {
+                OAuth2TokenType tokenType = context.getTokenType();
+
+                if (OAuth2TokenType.ACCESS_TOKEN.equals(tokenType)) {
+                    var authorities = authentication.getAuthorities();
+
+                    List<String> authoritieslist = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+
+                    context
+                            .getClaims()
+                            .claim("authorities", authoritieslist)
+                            .claim("email", authentication.getUsuario().getEmail());
+                }
+            }
+
+        };
     }
 }
